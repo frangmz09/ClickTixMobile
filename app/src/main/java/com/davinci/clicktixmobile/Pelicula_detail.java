b29b879543825bed7aeb0c506e9644e6119bd281
TiagoWalterStansiola <144070127+TiagoWalterStansiola@users.noreply.github.com>
2023-10-22 18:59:26 -0300

    feat: ultimos detalles en activity_pelicula_detail falta boton_back

---
+++ app/src/main/java/com/davinci/clicktixmobile/Pelicula_detail.java
@@ -0,0 +1,14 @@
+package com.davinci.clicktixmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pelicula_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pelicula_detail);
+    }
+}
