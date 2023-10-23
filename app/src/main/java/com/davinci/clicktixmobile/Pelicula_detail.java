0aff6a750eb7deee8e98fe2bdf120cb465fca61f
TiagoWalterStansiola <144070127+TiagoWalterStansiola@users.noreply.github.com>
2023-10-23 19:54:21 -0300

    feat: evento de boton back en activity_pelicula_detail

--- app/src/main/java/com/davinci/clicktixmobile/Pelicula_detail.java
+++ app/src/main/java/com/davinci/clicktixmobile/Pelicula_detail.java
@@ -2,7 +2,11 @@ package com.davinci.clicktixmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Pelicula_detail extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class Pelicula_detail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pelicula_detail);
+
+        ImageButton backButton = findViewById(R.id.btnVolverCartelera);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Pelicula_detail.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
