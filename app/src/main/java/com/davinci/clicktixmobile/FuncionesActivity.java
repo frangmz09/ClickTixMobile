7e1c8b2cd1837c66a017687742d8bd5b6143b1b7
JuampiLampu <144070275+JuampiLampu@users.noreply.github.com>
2023-11-30 18:57:27 -0300

    feat: activity_funciones intent a payment

--- app/src/main/java/com/davinci/clicktixmobile/FuncionesActivity.java
+++ app/src/main/java/com/davinci/clicktixmobile/FuncionesActivity.java
@@ -1,17 +1,27 @@
 package com.davinci.clicktixmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+public class FuncionesActivity extends AppCompatActivity {
 
-    public class FuncionesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_funciones);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_funciones);
-
-
-        }
+        Button btnSiguiente = findViewById(R.id.btn_siguiente);
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para pasar de FuncionesActivity a ActivityPayment
+                Intent intent = new Intent(FuncionesActivity.this, PaymentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+}
