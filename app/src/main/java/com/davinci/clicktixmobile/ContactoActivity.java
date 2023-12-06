2d29f1db45b0af7a20f638184c19bb37434b1880
JuampiLampu <144070275+JuampiLampu@users.noreply.github.com>
2023-12-05 21:15:29 -0300

    feat: arreglos

--- app/src/main/java/com/davinci/clicktixmobile/ContactoActivity.java
+++ app/src/main/java/com/davinci/clicktixmobile/ContactoActivity.java
@@ -2,7 +2,9 @@ package com.davinci.clicktixmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ContactoActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ContactoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+    }
+    public void openMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
